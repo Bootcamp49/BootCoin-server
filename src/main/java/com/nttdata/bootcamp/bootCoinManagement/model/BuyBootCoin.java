19cca58910f4c3f0a2c06212d454b8d89f725627
Flavio Josué Castillo Farías <fcastilf@emeal.nttdata.com>
2024-02-15 11:55:04 -0500

    Updated buyBootCoin body to add lombok tags

--- src/main/java/com/nttdata/bootcamp/bootCoinManagement/model/BuyBootCoin.java
+++ src/main/java/com/nttdata/bootcamp/bootCoinManagement/model/BuyBootCoin.java
@@ -1,5 +1,14 @@
 package com.nttdata.bootcamp.bootCoinManagement.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BuyBootCoin {
     private String clientIdBuy;
     private Double amount;
